67f54acfc896d21372f5f075d0ab83bcb379359c
Kyle <gouldielox33@gmail.com>
2023-08-07 10:22:02 -0400

    complete

--- script.js
+++ script.js
@@ -1,117 +1,130 @@
-// Menu data structure
 var menuLinks = [
-  {text: 'about', href: '/about'},
-  {text: 'catalog', href: '#', subLinks: [
-    {text: 'all', href: '/catalog/all'},
-    {text: 'top selling', href: '/catalog/top'},
-    {text: 'search', href: '/catalog/search'},
-  ]},
-  {text: 'orders', href: '#' , subLinks: [
-    {text: 'new', href: '/orders/new'},
-    {text: 'pending', href: '/orders/pending'},
-    {text: 'history', href: '/orders/history'},
-  ]},
-  {text: 'account', href: '#', subLinks: [
-    {text: 'profile', href: '/account/profile'},
-    {text: 'sign out', href: '/account/signout'},
-  ]},
+  { text: "about", href: "/about" },
+  {
+    text: "catalog",
+    href: "#",
+    subLinks: [
+      { text: "all", href: "/catalog/all" },
+      { text: "top selling", href: "/catalog/top" },
+      { text: "search", href: "/catalog/search" },
+    ],
+  },
+  {
+    text: "orders",
+    href: "#",
+    subLinks: [
+      { text: "new", href: "/orders/new" },
+      { text: "pending", href: "/orders/pending" },
+      { text: "history", href: "/orders/history" },
+    ],
+  },
+  {
+    text: "account",
+    href: "#",
+    subLinks: [
+      { text: "profile", href: "/account/profile" },
+      { text: "sign out", href: "/account/signout" },
+    ],
+  },
 ];
 
 let mainEl = document.querySelector("main");
-mainEl.style.backgroundColor = "#4a4e4d";
+let topMenuEl = document.getElementById("top-menu");
+let subMenuEl = document.getElementById("sub-menu");
+
+
+mainEl.style.backgroundColor = "var(--main-bg)";
+mainEl.innerHTML = "<h1>SEI Rocks!</h1>";
 mainEl.classList.add("flex-ctr");
 
-let topMenuEl = document.querySelector("#top-menu");
+
 topMenuEl.style.height = "100%";
-topMenuEl.style.backgroundColor = "#0e9aa7";
+topMenuEl.style.backgroundColor = "var(--top-menu-bg)";
 topMenuEl.classList.add("flex-around");
 
-for(let i = 0; i < menuLinks.length; i++) {
-  const menuLink = menuLinks[i];
+menuLinks.forEach(function (aTag) {
+  let aEl = document.createElement("a");
+  aEl.textContent = aTag.text;
+  aEl.setAttribute("href", aTag.href);
+  topMenuEl.appendChild(aEl);
+  return;
+});
 
-const aEl = document.createElement("a");
-aEl.textContent = menuLink.text;
-aEl.setAttribute("href", menuLink.href);
-topMenuEl.appendChild(aEl);
-}
 
-let subMenuEl = document.querySelector("#sub-menu");
 subMenuEl.style.height = "100%";
-subMenuEl.style.backgroundColor = "#3da4ab";
+subMenuEl.style.backgroundColor = "var(--sub-menu-bg)";
 subMenuEl.classList.add("flex-around");
 subMenuEl.style.position = "absolute";
 subMenuEl.style.top = "0";
 
-let topMenuLinks = document.querySelectorAll("topMenuEl");
+let topMenuLinks = document.querySelectorAll("a");
+var showingSubMenu = false;
+topMenuEl.addEventListener("click", topMenuClick);
 
-topMenuEl.addEventListener("click", (event) => {
-  event.preventDefault();
-  if (event.target.tagName !== "A") {
-    return;
-  }
-  console.log(event.target.textContent);
+function topMenuClick(evt) {
+  evt.preventDefault();
 
-  if (event.target.classList.contains("active")) {
-    event.target.classList.remove("active");
+  if (evt.target.tagName !== "A") return;
+
+  if (evt.target.classList.contains("active")) {
+    evt.target.classList.remove("active");
     showingSubMenu = false;
-    subMenuEl.style.top = "0";
+    subMenuEl.style.top = 0;
     return;
   }
-
   for (let i = 0; i < topMenuLinks.length; i++) {
     topMenuLinks[i].classList.remove("active");
   }
-  event.target.classList.add("active");
+  if (evt.target) evt.target.classList.add("active");
 
-  const anchorName = event.target.textContent;
-  const menuLink = menuLinks.find((link) => {
+  menuLinks.forEach(function (sub) {
+    if (sub.subLinks !== undefined) showingSubMenu = true;
+    else showingSubMenu = false;
   });
 
-  if (menuLink === undefined) {
-    return;
-  }
-
-  if (menuLink.subLinks) {
-    showingSubMenu = true;
-  } else {
-    showingSubMenu = false;
-  }
-
-  const buildSubMenu = (sublinks) => {
-    subMenuEl.innerHTML = "";
-    for (let i = 0; i < sublinks.length; i++) {
-      const newAnchor = document.createElement("a");
-      newAnchor.setAttribute("href", sublinks[i].href);
-      newAnchor.textContent = sublinks[i].text;
-      subMenuEl.append(newAnchor);
-    }
-  };
-
-  if (showingSubMenu) {
-    buildSubMenu(menuLink.subLinks);
-    subMenuEl.style.top = "100%";
-  } else {
+  if (showingSubMenu === true) buildSubMenu();
+  else {
     subMenuEl.style.top = "0";
   }
 
-  if (anchorName === "about") {
-    mainEl.innerHTML = "<h1>about</h1>";
-  }
-});
+  function buildSubMenu(i, j) {
+    let currentLink;
+    currentContent = subMenuEl.querySelectorAll("a");
 
-subMenuEl.addEventListener("click", (event) => {
-  event.preventDefault();
-  if (event.target.tagName !== "A") {
+    if (currentContent.length !== 0) {
+      currentContent.forEach(function (child) {
+        subMenuEl.removeChild(child);
+      });
+    }
+    for (i = 0; i < menuLinks.length; i++) {
+      currentLink = menuLinks[i].text;
+      current = menuLinks[i];
+      if (
+        evt.target.innerHTML === currentLink &&
+        evt.target.innerHTML !== "ABOUT"
+      ) {
+        for (j = 0; j < current.subLinks.length; j++) {
+          let subMenuObj = document.createElement("a");
+          subMenuObj.textContent = current.subLinks[j].text;
+          subMenuObj.setAttribute("href", current.subLinks[j].href);
+          subMenuEl.appendChild(subMenuObj);
+          subMenuEl.style.top = "100%";
+        }
+      }
+    }
     return;
   }
-  console.log(event.target.textContent);
+}
+subMenuEl.addEventListener("click", subMenuClick);
+function subMenuClick(evt) {
+  evt.preventDefault();
+  if (evt.target.tagName !== "A") console.log(evt.target);
 
   showingSubMenu = false;
   subMenuEl.style.top = "0";
-
   for (let i = 0; i < topMenuLinks.length; i++) {
     topMenuLinks[i].classList.remove("active");
+    if (evt.target.innerText === "ABOUT") mainEl.innerHTML = "ABOUT";
+    else mainEl.innerHTML = `<h1>${evt.target.innerText}</h1>`;
   }
-
-  mainEl.innerHTML = `<h1>${event.target.textContent}</h1>`;
-});
+}
